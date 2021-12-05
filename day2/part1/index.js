900b6882328672038a86e130930521dfdbf95438
Kyle Peeler <kylepeeler@hey.com>
2021-12-05 13:05:55 -0500

    finished day2 part 1

---
+++ day2/part1/index.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+
+try {
+  const data = fs.readFileSync('./input', 'utf8');
+  const lineData = data.split('\n');
+
+  const commands = lineData.map(line => line.split(' ')[0]);
+  const coordinates = lineData.map(line => line.split(' ')[1]);
+
+  let horizontalPosition = 0;
+  let depth = 0;
+
+  for (let i = 0; i < commands.length; i++) {
+    switch (commands[i]) {
+      case 'forward':
+        horizontalPosition += Number(coordinates[i]);
+        break;
+      case 'down':
+        depth += Number(coordinates[i]);
+        break;
+      case 'up':
+        depth -= Number(coordinates[i]);
+        break;
+      default:
+        break;
+    }
+  }
+
+  console.log('got depth as ', depth);
+  console.log('got horizontal position as ', horizontalPosition);
+
+  console.log('got puzzle solution as ', depth * horizontalPosition)
+
+} catch (err) {
+  console.error(err)
+}
